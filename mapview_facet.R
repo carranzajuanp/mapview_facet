c1beb55bbb5c735d93286a2d36ee6db01b8ef274
Juan Pablo Carranza <63171088+carranzajuanp@users.noreply.github.com>
2021-02-11 16:37:39 -0300

    Update mapview_facet.R

--- mapview_facet.R
+++ mapview_facet.R
@@ -2,7 +2,7 @@ mapview_facet <- function(x,f) {
   
   criteria=split(x,x[[f]])
   for (i in 1:length(criteria)) {
-    map=mapview(criteria[[i]])
+    map=mapview::mapview(criteria[[i]])
     assign(paste0("map_",i), map)
   }
   set=list(map_1)
