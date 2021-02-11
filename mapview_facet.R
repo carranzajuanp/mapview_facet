b9aa95a0dff9a62ca5512ef88c1556d0cd52a16b
Juan Pablo Carranza <63171088+carranzajuanp@users.noreply.github.com>
2021-02-11 16:24:43 -0300

    Add files via upload

---
+++ mapview_facet.R
@@ -0,0 +1,18 @@
+mapview_facet <- function(x,f) {
+  
+  criteria=split(x,x[[f]])
+  for (i in 1:length(criteria)) {
+    map=mapview(criteria[[i]])
+    assign(paste0("map_",i), map)
+  }
+  set=list(map_1)
+  for (i in 2:length(criteria)) {set=append(set, get(paste0("map_",i)))}
+  leafsync::latticeView(set)
+}
+
+mapview_facet(x = df, f = "var")
+
+
+
+
+
